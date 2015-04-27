b4860ff12fae8254326849c59fa3a9fc07fb81d0
Tongliang Liu <tliu3@illumina.com>
2015-04-26 17:48:30 -0700

    Added a custom Jackson2ObjectMapperBuilder, overriding the auto-configured one.

--- spring-doge/src/main/java/com/tliu3/demo/springboot/SpringDogeApplication.java
+++ spring-doge/src/main/java/com/tliu3/demo/springboot/SpringDogeApplication.java
@@ -2,11 +2,25 @@ package com.tliu3.demo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @SpringBootApplication
 public class SpringDogeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringDogeApplication.class, args);
-    }
+	@Bean
+	Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+		builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+		// Serialize `LocalDateTime` to string; otherwise it'll be serialized to an array
+		builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		return builder;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDogeApplication.class, args);
+	}
 }
